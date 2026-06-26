43ef0d88e0bbf8224781d5105293d57687c65071
TerrorBhai <152867529+vickyhubhai@users.noreply.github.com>
2026-06-27 01:10:56 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,9 +8,10 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
-const GA_MEASUREMENT_ID = "G-61LXYKCHXT";
+
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
+const GA_MEASUREMENT_ID = "G-61LXYKCHXT";
 
 function NotFoundComponent() {
   return (
