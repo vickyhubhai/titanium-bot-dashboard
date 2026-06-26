b5a03eae7ded3c8c91d32831719b309a4b577bc2
TerrorBhai <152867529+vickyhubhai@users.noreply.github.com>
2026-06-27 01:05:04 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,7 +8,7 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
-
+const GA_MEASUREMENT_ID = "G-61LXYKCHXT";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
